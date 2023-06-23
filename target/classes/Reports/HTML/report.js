69d158fc4974a0506e5389f160df8a1cc230d3a4
Neerajkumawat99 <39545182+Neerajkumawat99@users.noreply.github.com>
2023-06-23 12:11:41 +0530

    New_Contact_Updated_Code

---
+++ target/classes/Reports/HTML/report.js
@@ -0,0 +1,175 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/main/resources/Features/New_Contact.feature");
+formatter.feature({
+  "name": "Validate New Contact UI job upload",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "To Validate SalesIntel Orchestration Login Functionality",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@First"
+    },
+    {
+      "name": "@Sanity"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Launch The URL",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "Steps.launch_the_url()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter The User Name",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Steps.enter_The_User_Name()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter The Password",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.enter_The_Password()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click On Remember Me Check Box",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.click_On_Remember_Me_Check_Box()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click On Submit Button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.click_On_Submit_Button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "To Validate SalesIntel Orchestration New Contact Functionality",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@First"
+    },
+    {
+      "name": "@Sanity"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click On Job Label",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Steps.click_On_Job_Label()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click On New Contact Button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.click_on_new_contact_button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click On Create New Job",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.click_On_Create_New_Job()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter Job Name In New Contact Job Page",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.Enter_job_name()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Select Priority",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.Select_Priority()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Select On Screen Entry",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.Select_On_Screen_Entry()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter Linkedin URl",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.Enter_Linkedin_URl()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click On Submit Button",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.click_On_Submit_Button()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+});
